5d27eb1626cbce920a80147eca9c759f34002586
storminator89 <30489872+storminator89@users.noreply.github.com>
2023-03-20 15:37:46 +0100

    Check Rezert 2023

--- scripts/desktop/testGitStart/myOnload.js
+++ scripts/desktop/testGitStart/myOnload.js
@@ -2,3 +2,8 @@ function myOnload() {
     jr_notify_success('Hello Git!');
     jr_notify_success('Push aus Github2');
 }
+
+function greetingAlos(){
+    
+    var greeting = 'Training 20.03.2023'
+}
